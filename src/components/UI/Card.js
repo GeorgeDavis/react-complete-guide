497594a5cdac17a2a8e32b94ce2b9247cc0b5a49
George Davis <george.davis@pm-mac-1256.lan>
2022-12-29 18:22:27 -0500

    Refactored code to use ES6 syntax

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,6 +1,6 @@
 import './Card.css'
 
-function Card (props) {
+const Card = (props) => {
     const classes = 'card ' + props.className;
     // children is a reserved name. The value of the children will always be the output of the content between the open & closing tags - aka the Card
     return <div className={classes}>{props.children}</div>
